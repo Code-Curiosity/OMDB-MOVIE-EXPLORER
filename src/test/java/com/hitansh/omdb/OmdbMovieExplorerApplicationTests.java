160dec223cc083e841a8c483dc9ce97b8c84631f
code-curiosity <hitanshbhagtani@gmail.com>
2025-11-24 22:44:40 +0530

    Created the project structure using start.spring.io

---
+++ src/test/java/com/hitansh/omdb/OmdbMovieExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitansh.omdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbMovieExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
